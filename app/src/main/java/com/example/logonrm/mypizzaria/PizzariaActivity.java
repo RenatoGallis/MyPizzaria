5fa7a2044d9fbf2d537acbb878ec5db2c88b618e
logonrm <logonrm@fiap.com.br>
2017-05-29 21:19:53 -0300

    Initial commit

---
+++ app/src/main/java/com/example/logonrm/mypizzaria/PizzariaActivity.java
@@ -0,0 +1,13 @@
+package com.example.logonrm.mypizzaria;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PizzariaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pizzaria);
+    }
+}
